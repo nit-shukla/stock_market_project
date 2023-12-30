ac4ca939aef87ce8f4a4b04f883094cd4531a7e2
nit-shukla <shuklanitin2000@gmail.com>
2023-12-30 12:29:50 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:untitled2/Screens/controller/Firebase_Api.dart';
@@ -8,9 +7,9 @@ import 'package:untitled2/Screens/controller/Firebase_Api.dart';
 import 'Screens/Onboarding.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  await FirebaseApi().initNotifications();
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+  // await FirebaseApi().initNotifications();
   runApp(const MyApp());
 }
 
